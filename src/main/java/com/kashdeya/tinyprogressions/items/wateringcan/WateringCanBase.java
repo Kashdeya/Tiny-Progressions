2baa4667cdfdc55bfcefdcc95ce5feccd9fee4c7
Tom Wright <tomawright95@outlook.com>
2017-08-15 18:47:01 +0100

    WateringCanBase amended to allow shift-clicking watering

--- src/main/java/com/kashdeya/tinyprogressions/items/wateringcan/WateringCanBase.java
+++ src/main/java/com/kashdeya/tinyprogressions/items/wateringcan/WateringCanBase.java
@@ -3,6 +3,7 @@ package com.kashdeya.tinyprogressions.items.wateringcan;
 import java.util.Random;
 
 import com.kashdeya.tinyprogressions.handlers.CanHandler;
+import com.kashdeya.tinyprogressions.inits.TechItems;
 import com.kashdeya.tinyprogressions.main.TinyProgressions;
 
 import net.minecraft.block.Block;
@@ -13,6 +14,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.EnumAction;
+import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
@@ -20,139 +22,203 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.RayTraceResult.Type;
 import net.minecraft.world.World;
 import net.minecraftforge.event.ForgeEventFactory;
 
-public class WateringCanBase extends Item {
-	
-		private int range = 1;
-		private int waterChance = 25;
-		
-		private boolean canWater = false;
-		private boolean showParticlTicks = false; 
-		
-		private long ticksInUse;
-		
-		public WateringCanBase(){
-			this.setMaxStackSize(1);
-			this.setCreativeTab(TinyProgressions.tabTP);
-		}
-		
-		protected void setWateringRange(int newRange)
-		{
-			this.range = newRange;
-		}
-		
-		protected void setWateringChance(int newChance)
-		{
-			this.waterChance = newChance;
-		}
+public class WateringCanBase extends Item
+{
 
-	    public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected)
-	    {
-	    	if(isSelected){
-	    		ticksInUse++;
+    private int     range            = 1;
+    private int     waterChance      = 25;
 
-	    		if(ticksInUse % 4 == 0)
-	    		{
-	    			this.showParticlTicks = true;
-	    			canWater = true;
-	    		}
-	    	}
-	    }
+    private boolean canWater         = false;
+    private boolean showParticlTicks = false;
 
-	    @Override
-	    public EnumAction getItemUseAction(ItemStack stack)
-	    {
-	        return EnumAction.NONE;
-	    }
-	    
-	    int clicks = 0;
-	    
-	    @Override
-	    public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
-	    {
-			if (!player.canPlayerEdit(pos.offset(facing), facing, player.getHeldItem(hand)))
-		    {
-		      return EnumActionResult.FAIL;
-		    }
-			
-			//Dont show particals if you cant water yet. 
-			if(this.showParticlTicks){
-    			this.showParticlTicks = false;
+    private boolean forceActive      = false;
 
-				Random rand = new Random();
-				for (int x = -range; x <= range; x++) {
-					for (int z = -range; z <= range; z++)
-					{
-						double d0 = pos.add(x, 0, z).getX() + rand.nextFloat();
-						double d1 = pos.add(x, 0, z).getY() + 1.0D;
-						double d2 = pos.add(x, 0, z).getZ() + rand.nextFloat();
-		        
-						IBlockState checkSolidState = world.getBlockState(pos);
-						Block checkSolid = checkSolidState.getBlock();
-						if ((checkSolid.isFullCube(checkSolidState)) || ((checkSolid instanceof BlockFarmland))) {
-							d1 += 1.0D;
-						}
-		        
-						world.spawnParticle(EnumParticleTypes.WATER_DROP, d0, d1, d2, 0.0D, 0.0D, 0.0D, new int[5]);
-					}
-				}
-			}
-			
-			// only tick if canWater is true (Basically after the duration runs out getMaxDuration)
-		    if (!world.isRemote && canWater)
-		    {
-		    	canWater = false;
-		    	int chance = CanHandler.randInt(1, 100);
-		        if (chance <= waterChance)
-		        {
-		          for (int xAxis = -range; xAxis <= range; xAxis++) {
-		            for (int zAxis = -range; zAxis <= range; zAxis++) {
-		              for (int yAxis = -range; yAxis <= range; yAxis++)
-		              {
-		            	  Block checkBlock = world.getBlockState(pos.add(xAxis, yAxis, zAxis)).getBlock();
-		            	  
-		            	  if(checkBlock instanceof IGrowable || checkBlock == Blocks.MYCELIUM || checkBlock == Blocks.CACTUS || checkBlock == Blocks.REEDS || checkBlock == Blocks.CHORUS_FLOWER)
-		            	  {
-		            		  world.scheduleBlockUpdate(pos.add(xAxis, yAxis, zAxis), checkBlock, 0, 1);
-		            	  }
-		              }
-		            }
-		          }
-		          return EnumActionResult.FAIL;
-		        }
-		        //return EnumActionResult.FAIL;
-		      }
-		      return EnumActionResult.FAIL;
-		    }
-		
-		public static boolean applyBonemeal(ItemStack stack, World worldIn, BlockPos target, EntityPlayer player, EnumHand hand){
-			
-		        IBlockState iblockstate = worldIn.getBlockState(target);
+    private long    ticksInUse;
 
-		        int hook = ForgeEventFactory.onApplyBonemeal(player, worldIn, target, iblockstate, stack, hand);
-		        if (hook != 0) return hook > 0;
+    public WateringCanBase()
+    {
+        this.setMaxStackSize(1);
+        this.setCreativeTab(TinyProgressions.tabTP);
+    }
 
-		        if ((iblockstate.getBlock() instanceof IGrowable && iblockstate.getBlock() != Blocks.GRASS))
-		        {
-		            IGrowable igrowable = (IGrowable)iblockstate.getBlock();
+    protected void setWateringRange(int newRange)
+    {
+        this.range = newRange;
+    }
 
-		            if (igrowable.canGrow(worldIn, target, iblockstate, worldIn.isRemote))
-		            {
-		                if (!worldIn.isRemote)
-		                {
-		                    if (igrowable.canUseBonemeal(worldIn, worldIn.rand, target, iblockstate))
-		                    {
-		                        igrowable.grow(worldIn, worldIn.rand, target, iblockstate);
-		                    }
+    protected void setWateringChance(int newChance)
+    {
+        this.waterChance = newChance;
+    }
+    
+    public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected)
+    {
+        ticksInUse++;
 
-		                    stack.setCount(stack.getCount() - 1);
-		                }
+        if(ticksInUse % 4 == 0)
+        {
+            showParticlTicks = true;
+            canWater = true;
+        }
+        
+        if(forceActive && entityIn instanceof EntityPlayer)
+        {
+            EntityPlayer player = (EntityPlayer)entityIn;
+            
+            if(!isSelected)
+            {
+                ItemStack offhand = player.getHeldItem(EnumHand.OFF_HAND);
+                
+                if(offhand.getItem() != TechItems.watering_can && offhand.getItem() != TechItems.watering_can_upgrade)
+                    forceActive = false;
+            }
+            
+            RayTraceResult raytrace = rayTrace(worldIn, player, false);
+            
+            if(raytrace != null && raytrace.typeOfHit == Type.BLOCK)
+            {
+                attemptWaterParticles(worldIn, raytrace.getBlockPos());
+                attemptWater(worldIn, raytrace.getBlockPos());
+            }
+        }
+    }
 
-		                return true;
-		            }
-		        }
-		        return false;
-		    }
+    @Override
+    public EnumAction getItemUseAction(ItemStack stack)
+    {
+        return EnumAction.NONE;
+    }
+
+    @Override
+    public EnumRarity getRarity(ItemStack stack)
+    {
+        return hasEffect(stack) ? EnumRarity.UNCOMMON : EnumRarity.COMMON;
+    }
+
+    @Override
+    public boolean hasEffect(ItemStack stack)
+    {
+        return forceActive;
+    }
+
+    int clicks = 0;
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing,
+            float hitX, float hitY, float hitZ)
+    {
+        if(!world.isRemote && player.isSneaking())
+        {
+            forceActive = !forceActive;
+            return EnumActionResult.FAIL;
+        }
+
+        if(!player.canPlayerEdit(pos.offset(facing), facing, player.getHeldItem(hand)))
+        {
+            return EnumActionResult.FAIL;
+        }
+
+        attemptWaterParticles(world, pos);
+        return attemptWater(world, pos);
+    }
+
+    private EnumActionResult attemptWater(World world, BlockPos pos)
+    {
+        // only tick if canWater is true (Basically after the duration runs out
+        // getMaxDuration)
+        if(!world.isRemote && canWater)
+        {
+            canWater = false;
+            int chance = CanHandler.randInt(1, 100);
+            if(chance <= waterChance)
+            {
+                for(int xAxis = -range; xAxis <= range; xAxis++)
+                {
+                    for(int zAxis = -range; zAxis <= range; zAxis++)
+                    {
+                        for(int yAxis = -range; yAxis <= range; yAxis++)
+                        {
+                            Block checkBlock = world.getBlockState(pos.add(xAxis, yAxis, zAxis)).getBlock();
+
+                            if(checkBlock instanceof IGrowable || checkBlock == Blocks.MYCELIUM
+                                    || checkBlock == Blocks.CACTUS || checkBlock == Blocks.REEDS
+                                    || checkBlock == Blocks.CHORUS_FLOWER)
+                            {
+                                world.scheduleBlockUpdate(pos.add(xAxis, yAxis, zAxis), checkBlock, 0, 1);
+                            }
+                        }
+                    }
+                }
+                return EnumActionResult.FAIL;
+            }
+            // return EnumActionResult.FAIL;
+        }
+        return EnumActionResult.FAIL;
+    }
+    
+    private void attemptWaterParticles(World world, BlockPos pos)
+    {
+        // Dont show particals if you cant water yet.
+        if(this.showParticlTicks)
+        {
+            this.showParticlTicks = false;
+
+            Random rand = new Random();
+            for(int x = -range; x <= range; x++)
+            {
+                for(int z = -range; z <= range; z++)
+                {
+                    double d0 = pos.add(x, 0, z).getX() + rand.nextFloat();
+                    double d1 = pos.add(x, 0, z).getY() + 1.0D;
+                    double d2 = pos.add(x, 0, z).getZ() + rand.nextFloat();
+
+                    IBlockState checkSolidState = world.getBlockState(pos);
+                    Block checkSolid = checkSolidState.getBlock();
+                    if((checkSolid.isFullCube(checkSolidState)) || ((checkSolid instanceof BlockFarmland)))
+                    {
+                        d1 += 1.0D;
+                    }
+
+                    world.spawnParticle(EnumParticleTypes.WATER_DROP, d0, d1, d2, 0.0D, 0.0D, 0.0D, new int[5]);
+                }
+            }
+        }
+    }
+
+    public static boolean applyBonemeal(ItemStack stack, World worldIn, BlockPos target, EntityPlayer player,
+            EnumHand hand)
+    {
+
+        IBlockState iblockstate = worldIn.getBlockState(target);
+
+        int hook = ForgeEventFactory.onApplyBonemeal(player, worldIn, target, iblockstate, stack, hand);
+        if(hook != 0)
+            return hook > 0;
+
+        if((iblockstate.getBlock() instanceof IGrowable && iblockstate.getBlock() != Blocks.GRASS))
+        {
+            IGrowable igrowable = (IGrowable)iblockstate.getBlock();
+
+            if(igrowable.canGrow(worldIn, target, iblockstate, worldIn.isRemote))
+            {
+                if(!worldIn.isRemote)
+                {
+                    if(igrowable.canUseBonemeal(worldIn, worldIn.rand, target, iblockstate))
+                    {
+                        igrowable.grow(worldIn, worldIn.rand, target, iblockstate);
+                    }
+
+                    stack.setCount(stack.getCount() - 1);
+                }
+
+                return true;
+            }
+        }
+        return false;
+    }
 }

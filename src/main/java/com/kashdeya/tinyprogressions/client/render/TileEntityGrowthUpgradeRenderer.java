f6feed7744682f0c68168ccd45172c1613397b9e
Tom <tomawright95@gmail.com>
2017-10-10 09:28:59 +0100

    Ported back vadis' render code from 1.12 to 1.11

--- src/main/java/com/kashdeya/tinyprogressions/client/render/TileEntityGrowthUpgradeRenderer.java
+++ src/main/java/com/kashdeya/tinyprogressions/client/render/TileEntityGrowthUpgradeRenderer.java
@@ -1,9 +1,7 @@
 package com.kashdeya.tinyprogressions.client.render;
 
-import com.kashdeya.tinyprogressions.inits.TechBlocks;
 import com.kashdeya.tinyprogressions.tiles.TileEntityGrowthUpgrade;
 
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelEnderCrystal;
 import net.minecraft.client.renderer.GlStateManager;
@@ -24,15 +22,13 @@ public class TileEntityGrowthUpgradeRenderer extends TileEntitySpecialRenderer<T
 
 	@Override
 	public void renderTileEntityAt(TileEntityGrowthUpgrade tile, double x, double y, double z, float partialTick, int destroyStage) {
-		IBlockState state = tile.getWorld().getBlockState(tile.getPos());
-
-		if(state == null || state.getBlock() != TechBlocks.growth_upgrade)
+		if(tile == null || !tile.hasWorld())
 			return;
 
 		float ticks = tile.animationTicks + (tile.animationTicks - tile.prevAnimationTicks) * partialTick;
 		GlStateManager.pushMatrix();
-		GlStateManager.translate(x + 0.5D, y + 0.45D, z + 0.5D);
-		GlStateManager.scale(0.4D, 0.35D, 0.4D);
+		GlStateManager.translate(x + 0.5D, y + 0.33D, z + 0.5D);
+		GlStateManager.scale(0.52D, 0.43D, 0.52D);
 		bindTexture(ENDER_CRYSTAL_TEXTURES);
 		modelEnderCrystalNoBase.render(entity, 0.0F, ticks * 3.0F, 0F, 0.0F, 0.0F, 0.0625F);
 		GlStateManager.popMatrix();

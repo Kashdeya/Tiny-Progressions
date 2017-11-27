5b60cf6bcfb2c69d3abac7cb2f42f48968b6bcf5
kashdeya <kashdeya@outlook.com>
2017-11-27 10:25:45 -0600

    fix juice to heal players only.

--- src/main/java/com/kashdeya/tinyprogressions/blocks/fluids/BlockFluidVasholine.java
+++ src/main/java/com/kashdeya/tinyprogressions/blocks/fluids/BlockFluidVasholine.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Random;
 
 import com.kashdeya.tinyprogressions.fluids.ModFluids;
-import com.kashdeya.tinyprogressions.handlers.ArmorHandler;
 import com.kashdeya.tinyprogressions.handlers.MaterialHandler;
 import com.kashdeya.tinyprogressions.main.TinyProgressions;
 
@@ -12,7 +11,9 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
@@ -27,6 +28,7 @@ public class BlockFluidVasholine extends BlockFluidClassic {
 	public BlockFluidVasholine() {
 		super(ModFluids.VASHOLINE, MaterialHandler.VASHOLINE);
 		setUnlocalizedName("vasholine");
+		this.setLightLevel(1.0F);
 	}
 
 	@Override
@@ -45,11 +47,14 @@ public class BlockFluidVasholine extends BlockFluidClassic {
 
 	@Override
 	public void onEntityCollidedWithBlock(World world, BlockPos pos, IBlockState state, Entity entity) {
-		if (entity instanceof EntityLivingBase) {
-			if(world.getTotalWorldTime()%20 == 0 && ((EntityLivingBase) entity).getHealth() < ((EntityLivingBase) entity).getMaxHealth()) // add whatever time you want here 20 = every 1 second
-				((EntityLivingBase) entity).heal(0.5F);
+		if (entity instanceof EntityPlayer) {
+			if(world.getTotalWorldTime()%20 == 0 && ((EntityPlayer) entity).getHealth() < ((EntityPlayer) entity).getMaxHealth()) // add whatever time you want here 20 = every 1 second
+				((EntityPlayer) entity).heal(0.5F);
 			entity.rotationYaw += (world.rand.nextFloat() - world.rand.nextFloat()) * 5.0D;
 			entity.rotationPitch += (world.rand.nextFloat() - world.rand.nextFloat()) * 5.0D;
+		}
+		if (entity instanceof EntityLivingBase && !(entity instanceof EntityPlayer)){
+			entity.attackEntityFrom(DamageSource.MAGIC	, 1.0F);
 		}
 	}
 

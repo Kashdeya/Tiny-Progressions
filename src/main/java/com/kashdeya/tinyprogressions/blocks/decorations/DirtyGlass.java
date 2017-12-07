3b0030211ea71d4b5a6d9c045fee6973bb2bfe6b
kashdeya <kashdeya@outlook.com>
2017-12-07 16:27:48 -0600

    dirty glass now drops when broke

--- src/main/java/com/kashdeya/tinyprogressions/blocks/decorations/DirtyGlass.java
+++ src/main/java/com/kashdeya/tinyprogressions/blocks/decorations/DirtyGlass.java
@@ -2,6 +2,7 @@ package com.kashdeya.tinyprogressions.blocks.decorations;
 
 import java.util.Random;
 
+import com.kashdeya.tinyprogressions.inits.TechBlocks;
 import com.kashdeya.tinyprogressions.main.TinyProgressions;
 import com.kashdeya.tinyprogressions.registry.utils.IOreDictEntry;
 
@@ -10,6 +11,7 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -53,7 +55,13 @@ public class DirtyGlass extends BlockGlass implements IOreDictEntry
     @Override
     public int quantityDropped(Random rand)
     {
-        return 0;
+        return 1;
+    }
+    
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune)
+    {
+        return Item.getItemFromBlock(TechBlocks.dirty_glass);
     }
     
     @Override

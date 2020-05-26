705abe5240e5b1a952382ab441ab32bc4726cec8
gendeathrow <gendeathrow@gmail.com>
2020-05-25 20:56:09 -0500

    Fixed some more broken recipes
    I broke stuff.. not its been fixed.

--- src/main/java/com/kashdeya/tinyprogressions/main/Reference.java
+++ src/main/java/com/kashdeya/tinyprogressions/main/Reference.java
@@ -4,10 +4,10 @@ public class Reference {
 	// Mod Constants
 	public static final String MOD_ID = "tp";
 	public static final String MOD_NAME = "Tiny Progressions";
-	public static final String VERSION = "4.0.0";
+	public static final String VERSION = "4.0.1";
 	
 	// Minecraft
-	public static final String MINECRAFT = "[1.14.4]";
+	public static final String MINECRAFT = "[1.15.4]";
 	
 	// Proxy Constants
 	public static final String PROXY_CLIENT = "com.kashdeya.tinyprogressions.proxy.ClientProxy";
